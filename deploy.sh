8816daf9c8da717c4dd58a3ca9a7484ef4248597
phucduong86 <phuc.duongdinh@gmail.com>
2019-01-08 23:15:08 -0500

    update about me

--- deploy.sh
+++ deploy.sh
@@ -11,25 +11,20 @@ git submodule add -b master git@github.com:phucduong86/phucduong86.github.io.git
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
+# Commit changes from public folder to website repo
 cd public
-# Add changes to git.
 git add .
 
-# Commit changes.
+# Commit changes to site.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
-
 # Clean up public folder
+cd ..
 rm -rf public
 
 # Commit changes to current repo.
